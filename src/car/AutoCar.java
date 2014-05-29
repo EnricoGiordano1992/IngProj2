011a67f6c9e83204990e72838f0833aa7469d36e
EnricoGiordano1992 <enricogiordano1992@gmail.com>
2014-05-29 15:53:11 +0200

    finite car

--- src/car/AutoCar.java
+++ src/car/AutoCar.java
@@ -2,8 +2,17 @@ package car;
 
 public class AutoCar extends Car {
 
+	private boolean break_signal;
+	
 	public AutoCar(){
 
 		super(10);
+		break_signal = false;
 	}
+	
+	public void setBreak_signal(boolean value){
+		
+		break_signal = value;
+	}
+	
 }
--- src/car/Car.java
+++ src/car/Car.java
@@ -1,16 +1,46 @@
 package car;
-import node.*;
 
-public class Car extends Node{
+public class Car {
 
 	private int speed_meter;
 	private String display;
-	private boolean break_signal;
+	private int p_rate;
 	
 	public Car(int p_rate){
-		super(p_rate);
+		p_rate = 0;
 		speed_meter = 0;
 		display = "";
-		break_signal = false;
 	}
+
+	public int getSpeed_meter() {
+		return speed_meter;
+	}
+
+	public void setSpeed_meter(int speed_meter) {
+		this.speed_meter = speed_meter;
+	}
+
+	public String getDisplay() {
+		return display;
+	}
+
+	public void setDisplay(String display) {
+		this.display = display;
+	}
+
+	public void setP_rate(int val){
+		
+		p_rate = val;
+	}
+	
+	
+	public void sendMessage(){
+		
+	}
+	
+	
+	public void receiveMessage(){
+		
+	}
+	
 }
