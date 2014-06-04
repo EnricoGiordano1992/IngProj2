a356fa7961a888367c3beb8675f40f21d3cde23c
albertomarini1992 <a_marini@hotmail.it>
2014-06-04 10:23:12 +0200

    station

---
+++ src/station/station.java
@@ -0,0 +1,22 @@
+package station;
+
+public class station {
+
+	private int pps;
+	private Comunication com;
+	
+	
+	
+	public void sendMessage(){
+		
+	}
+	
+	public void receiveMessage(){
+		
+	}
+	
+	public void update(){
+		
+	}
+
+}
